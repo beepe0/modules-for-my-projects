7c0d9f85e91d1825461d1bc8f08175fa308123ac
beepe0 <dshakhun@gmail.com>
2023-11-05 21:12:20 +0100

    Zaktualizowałem moduł unetserver

---
+++ Attributes/InputRuleAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Network.UnityServer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Delegate)]
+    public class InputRuleAttribute : Attribute
+    {
+        public InputRuleAttribute(UNetworkServer server)
+        {
+            
+        }
+    }
+}
---
+++ Attributes/OutputRuleAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Network.UnityServer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Delegate)]
+    public class OutputRuleAttribute : Attribute
+    {
+        public OutputRuleAttribute(UNetworkServer server)
+        {
+            
+        }
+    }
+}
