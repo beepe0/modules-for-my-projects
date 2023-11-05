8b00dcf36d8ce035255b07700de1d053d2b0bbf5
beepe0 <dshakhun@gmail.com>
2023-11-05 20:57:06 +0100

    Zaktualizowałem moduł unettools

---
+++ Attributes/InputRuleAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Network.UnityClient;
+using Network.UnityServer;
+
+namespace Network.UnityTools.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Delegate)]
+    public class InputRuleAttribute : Attribute
+    {
+        public InputRuleAttribute(UNetworkClient client)
+        {
+            
+        }
+        
+        public InputRuleAttribute(UNetworkServer server)
+        {
+            
+        }
+    }
+}
---
+++ Attributes/OutputRuleAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Network.UnityClient;
+using Network.UnityServer;
+
+namespace Network.UnityTools.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Delegate)]
+    public class OutputRuleAttribute : Attribute
+    {
+        public OutputRuleAttribute(UNetworkClient client)
+        {
+            
+        }
+        
+        public OutputRuleAttribute(UNetworkServer server)
+        {
+            
+        }
+    }
+}
