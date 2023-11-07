d8b84b7390a0c2268b67716dd978a06310e677e0
beepe0 <dshakhun@gmail.com>
2023-11-07 12:18:10 +0100

    Zaktualzowałem moduł unettools

--- Attributes/OutputRuleAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace Network.UnityTools.Attributes
-{
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Delegate)]
-    public class OutputRuleAttribute : Attribute {}
-}
